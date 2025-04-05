0452b86f6e8579d7114f2580c331fac2f81d6ad1
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-05 19:59:14 -0300

    Update ChargingStationsFile.py

--- app/Cloud/ChargingStationsFile.py
+++ app/Cloud/ChargingStationsFile.py
@@ -1,4 +1,4 @@
-# Esta Classe é responsável por armazenar os postos de recarga em um .json na Nuvem:
+# Esta Classe é Responsável por Armazenar os Dados dos Postos de Recarga em um Arquivo ".json" na Nuvem:
 
 import json
 import os
@@ -10,11 +10,11 @@ class ChargingStationsFile:
         self.json_file = json_file
         self.chargingStationsList = [] # Lista dos Postos de Recarga.
 
-    # Lendo os Pontos de Recarga no Arquivo .json:
+    # Lendo os Pontos de Recarga no Arquivo ".json":
     def readChargingStations(self):
         if os.path.exists(self.json_file):
             with open(self.json_file, "r", encoding="utf-8") as file:
-                self.chargingStationsList = json.load(file) # Salvando os Dados do Arquivo .json na Lista.
+                self.chargingStationsList = json.load(file) # Salvando os Dados do Arquivo ".json" na Lista.
     
     # Procurando um Posto de Recarga Específico:
     def findChargingStation(self, id):
@@ -26,11 +26,11 @@ class ChargingStationsFile:
             print(f"\nPosto de Recarga com ID '{id}' Não Foi Encontrado!\n")
             return None
 
-    # Listando os Postos de Recarga Cadastrados no Arquivo .json:
+    # Listando os Postos de Recarga Cadastrados no Arquivo ".json":
     def listChargingStations(self):
         return self.chargingStationsList
 
-    # Salvando a Lista no Arquivo .json:
+    # Salvando a Lista no Arquivo ".json":
     def saveChargingStations(self):
         with open(self.json_file, "w", encoding="utf-8") as file:
             json.dump(self.chargingStationsList, file, indent=4)
@@ -44,18 +44,18 @@ class ChargingStationsFile:
                 cs["y_position"] = y_position
                 updateStatus = True
         if updateStatus:
-            self.saveChargingStations() # Salvando no Arquivo .json.
+            self.saveChargingStations() # Salvando no Arquivo ".json".
             print(f"\nPosto de Recarga com ID '{id}' Foi Atualizado com Sucesso!\n")
         else:
             print(f"\nPosto de Recarga com ID '{id}' Não Foi Encontrado!\n")
     
-    # Criando um novo posto de recarga no arquivo .json:
+    # Criando um Novo Posto de Recarga no Arquivo ".json":
     def createChargingStation(self, id, x_position, y_position):
-        # Verificando se já existe um posto de recarga com mesmo ID cadastrado:
+        # Verificando Se Já Existe um Posto de Recarga Com Mesmo ID Cadastrado:
         if any(cs["id"] == id for cs in self.chargingStationsList): # Se Achar Pelo Menos um Com o Mesmo ID.
             print(f"\nJá Existe um Posto de Recarga com ID '{id}'!\n")
-        # Salvando o novo posto de recarga, se não existir:
+        # Salvando o Novo Posto de Recarga, Se Não Existir:
         else:
             self.chargingStationsList.append({"id": id, "x_position": x_position, "y_position": y_position}) # Salvando na Lista.
-            self.saveChargingStations() # Salvando no Arquivo .json.
+            self.saveChargingStations() # Salvando no Arquivo ".json".
             print(f"\nPosto de Recarga com ID '{id}' Foi Salvo com Sucesso!\n")
