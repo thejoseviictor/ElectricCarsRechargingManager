e0661628c75036824623de509337cbb9a8c6ff43
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-05 19:31:22 -0300

    Update Cloud.py

--- app/Cloud/Cloud.py
+++ app/Cloud/Cloud.py
@@ -1,8 +1,8 @@
 # Nuvem Que Será Responsável por Reservar Recargas Para os Carros:
 
 import socket
-from ChargingStationDAO import ChargingStationFile
-from Reservation import ReservationFile
+from ChargingStations import ChargingStationFile
+from Reservations import ReservationFile
 
 # Definindo o Host e Post da Nuvem:
 HOST = "0.0.0.0" # Aceita Conexões de Qualquer Dispositivo na Rede.
