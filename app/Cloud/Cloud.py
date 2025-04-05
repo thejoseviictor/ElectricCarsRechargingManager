8ab4ef01466cbe3a835310b2aaa420dc23d1eb46
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-05 19:19:45 -0300

    Update Cloud.py

--- app/Cloud/Cloud.py
+++ app/Cloud/Cloud.py
@@ -1,24 +1,32 @@
-#!/usr/bin/env python3
+# Nuvem Que Será Responsável por Reservar Recargas Para os Carros:
 
 import socket
 
-HOST = "0.0.0.0"
+# Definindo o Host e Post da Nuvem:
+HOST = "0.0.0.0" # Aceita Conexões de Qualquer Dispositivo na Rede.
 PORT = 65432
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP IPv4.
+# Definindo o Socket da Nuvem:
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Conexão TCP IPv4.
 server.bind((HOST, PORT)) # Definindo o IP e porta do socket.
-server.listen(1) # Escutando com uma fila de tamanho "1".
 
-print(f"\nListening on Port {PORT}...\n")
-
-conn, addr = server.accept()
-print(f"\nConnected to {addr}\n")
-
+# Inicia uma Conexão, Lê Seus Dados, Fecha a Conexão e Aguarda uma Nova Conexão:
 while True:
-    data = conn.recv(1024) # Dados de até 1024 bytes.
-    if not data:
-        break
-    print(f"\nReceived: {data.decode()}\n")
-    conn.sendall(b"Data Received Successfully") # Enviando confirmação ao cliente.
+    # Ativando o Modo de Escuta da Nuvem:
+    server.listen(1) # Escutando Com uma Fila de Tamanho "1".
+    print(f"\nOuvindo na Porta '{PORT}'...\n")
 
-conn.close()
+    # Aceitando uma Conexão:
+    conn, addr = server.accept()
+    print(f"\nConectado a '{addr}'\n")
+
+    # Mantendo no Loop Enquanto Dados do Dispositivo São Recebidos:
+    while True:
+        data = conn.recv(1024) # Dados de até 1024 bytes.
+        if not data:
+            break
+        print(f"\nReceived: {data.decode()}\n")
+        conn.sendall(b"Data Received Successfully") # Enviando confirmação ao cliente.
+
+    # Fechando a Conexão com o Dispositivo:
+    conn.close()
