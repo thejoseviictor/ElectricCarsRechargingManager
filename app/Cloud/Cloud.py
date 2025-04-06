178860e3da9a6350c5743f22e531a6e1711079e5
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-06 09:37:31 -0300

    Update Cloud.py

--- app/Cloud/Cloud.py
+++ app/Cloud/Cloud.py
@@ -1,6 +1,7 @@
 # Nuvem Que Será Responsável por Reservar Recargas Para os Carros:
 
 import socket
+import json
 from ChargingStationsFile import ChargingStationsFile
 from ReservationsFile import ReservationsFile
 
@@ -8,23 +9,40 @@ from ReservationsFile import ReservationsFile
 HOST = "0.0.0.0" # Aceita Conexões de Qualquer Dispositivo na Rede.
 PORT = 65432
 
+# Garantindo Que o Json Completo Seja Recebido:
+def receiveFullJson(server):
+    buffer = b"" # Variável de Bytes Vazia, Onde os Dados, em Pedaços, Serão Salvos.
+    # Loop de Recebimento do Json Completo:
+    while True:
+        chunk = server.recv(1024) # Recebendo os Dados em Partes de 1024 Bytes.
+        # Finalizando, Se Não Houverem Mais Dados a Receber:
+        if not chunk:
+            break
+        buffer += chunk # Somando os Dados no Buffer.
+        # Testando Se o Json Já Está Completo:
+        try:
+            return json.loads(buffer.decode()) # Decodificando os Dados em Dicionário (utf-8)
+        # Erro de Dados Incompletos:
+        except json.JSONDecodeError:
+            continue
+
 # Definindo o Socket da Nuvem:
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Conexão TCP IPv4.
-server.bind((HOST, PORT)) # Definindo o IP e porta do socket.
+cloud = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Conexão TCP IPv4.
+cloud.bind((HOST, PORT)) # Definindo o IP e porta do socket.
 
 # Inicia uma Conexão, Lê Seus Dados, Fecha a Conexão e Aguarda uma Nova Conexão:
 while True:
     # Ativando o Modo de Escuta da Nuvem:
-    server.listen(1) # Escutando Com uma Fila de Tamanho "1".
+    cloud.listen(1) # Escutando Com uma Fila de Tamanho "1".
     print(f"\nOuvindo na Porta '{PORT}'...\n")
 
     # Aceitando uma Conexão:
-    conn, addr = server.accept()
+    conn, addr = cloud.accept()
     print(f"\nConectado a '{addr}'\n")
 
     # Mantendo no Loop Enquanto Dados do Dispositivo São Recebidos:
     while True:
-        data = conn.recv(1024) # Dados de até 1024 bytes.
+        data = receiveFullJson(cloud)
         if not data:
             break
         print(f"\nReceived: {data.decode()}\n")
