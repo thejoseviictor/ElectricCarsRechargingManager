59e3b7d0b6dae4b1690cb0d0afe0e1ef6e091253
João Macedo <joao.macedo.lima@hotmail.com>
2025-04-06 10:00:06 -0300

    Delete others directories

--- app/Vehicle/Vehicle.py
+++ app/Vehicle/Vehicle.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from app.Vehicle.User import User
-from app.ChargingPoint.Reservation import Reservation
 
 @dataclass
 class Vehicle:
@@ -37,6 +36,3 @@ class Vehicle:
     def definePosition(self,x: int, y: int): # Método que define o localização do veículo
         self.coordinates.append(x)
         self.coordinates.append(y)
-    
-    def registerReservation(self, reservation: Reservation): # Método para registrar um carregamento finalizado.
-        self.reservationHistory.append(reservation)
