f342f8d8fe9998e2a51e6491ab0f9568faf64fc7
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-06 10:16:56 -0300

    Update ReservationsFile.py

--- app/Cloud/ReservationsFile.py
+++ app/Cloud/ReservationsFile.py
@@ -2,49 +2,54 @@
 
 import json
 import os
+import datetime
 
 class Reservation:
     # Inicializando a Classe e seus Atributos:
-    def __init__(self, ID, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID):
+    def __init__(self, ID, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID, actualBatteryPercentage, batteryCapacity):
         self.ID = ID    # ID da Reserva.
         self.chargingStationID = chargingStationID  # ID do Posto de Recarga.
         self.chargingPointID = chargingPointID  # ID do Ponto de Carregamento.
-        self.chargingPointPower = chargingPointPower # Potência do Ponto de Carregamento em Watts.
+        self.chargingPointPower = chargingPointPower # Potência do Ponto de Carregamento em Watt.
         self.kWhPrice = kWhPrice    # Preço do kWh do Ponto de Carregamento.
         self.vehicleID = vehicleID  # ID do Veículo.
-        self.startDateTime = self.calculateStartDateTime()    # Formato da Data: DD/MM/AAAA-HH:MM (Dia, Mês, Ano, Hora, Minutos)
-        self.finishDateTime = self.calculateFinishDateTime()    # Formato da Data: DD/MM/AAAA-HH:MM (Dia, Mês, Ano, Hora, Minutos)
-        self.duration = self.calculeDuration()  # Duração da Recarga em Horas.
+        self.startDateTime = self.calculateStartDateTime()    # Formato: 0000-00-00T00:00:00 (Ano, Mês, Dia, T(Separador Entre Data e Hora), Hora, Minutos, Segundos)
+        self.finishDateTime = self.calculateFinishDateTime()    # Formato: 0000-00-00T00:00:00 (Ano, Mês, Dia, T(Separador Entre Data e Hora), Hora, Minutos, Segundos)
+        self.duration = self.calculeDuration(actualBatteryPercentage, batteryCapacity)  # Duração da Recarga em Horas.
         self.price = self.calculatePrice()  # Preço da Recarga.
 
     # Calculando o Preço da Recarga:
-    # kWh = (Potência do Carregador (Watts) * Tempo (Horas)) / 1000
+    # kWh = (Potência do Carregador (Watt) * Tempo (Horas)) / 1000
     def calculatePrice(self):
         kWh = (self.chargingPointPower * self.duration) / 1000
         return kWh * self.kWhPrice
 
-    # NÃO FINALIZADO!
-    def calculateDuration(self):
-        pass
+    # Calculando o Tempo para Completar a Carga de Bateria do Veículo:
+    # Tempo (Horas) = ((Carga Desejada - Carga Atual) * Capacidade da Bateria (kWh)) / Potência do Carregador (kW)
+    def calculateDuration(self, actualBatteryPercentage, batteryCapacity):
+        return ((100 - actualBatteryPercentage) * batteryCapacity) / (self.chargingPointPower / 1000)
 
     # NÃO FINALIZADO!
+    # Novas Reservas São Feitas para 5 Minutos Após a Última Reserva Cadastrada no Ponto de Carregamento:
     def calculateStartDateTime(self):
         pass
     
-    # NÃO FINALIZADO!
-    # Novas Reservas São Feitas para 5 Minutos Após a Última Reserva Cadastrada:
+    # Calcula a Data Que o Veículo Irá Terminar de Usar o Ponto de Carregamento, de Acordo com a Duração da Recarga em Horas:
+    # Data de Finalização = Data de Ínicio + Duração de Carregamento em Horas
     def calculateFinishDateTime(self):
-        pass
+        start = datetime.datetime.fromisoformat(self.startDateTime) # Decodificando a Data de Ínicio do Formato ISO para DateTime.
+        finish = start + datetime.timedelta(hours=self.duration) # Calculando a Data de Finalização.
+        return finish.isoformat() # Codificando a Data de Finalização do DateTime para Formato ISO.
 
+# Salvando as Reservas em um Arquivo ".json":
 class ReservationsFile: 
     # Inicializando a Classe e seus Atributos:
     def __init__(self, json_file="reservations.json"):
         self.json_file = json_file
         self.reservationsList = [] # Lista de Reservas.
     
-    # NÃO FINALIZADO!
     # Lendo as Reservas no Arquivo ".json":
-    def readReservation(self):
+    def readReservations(self):
         if os.path.exists(self.json_file):
             with open(self.json_file, "r", encoding="utf-8") as file:
                 self.reservationsList = json.load(file) # Salvando os Dados do Arquivo ".json" na Lista.
@@ -90,8 +95,8 @@ class ReservationsFile:
     
     # NÃO FINALIZADO!
     # Criando uma Reserva no Arquivo .json:
-    def createReservation(self, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID):
-        object = Reservation(id, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID)
+    def createReservation(self, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID, actualBatteryPercentage, batteryCapacity):
+        object = Reservation(id, chargingStationID, chargingPointID, chargingPointPower, kWhPrice, vehicleID, actualBatteryPercentage, batteryCapacity)
         # Verificando se já existe um posto de recarga com mesmo ID cadastrado:
         if any(cs["id"] == id for cs in self.chargingStationsList): # Se Achar Pelo Menos um Com o Mesmo ID.
             print(f"\nJá Existe um Posto de Recarga com ID '{id}'!\n")
