4e13642431ab83327f92931238ae48683b9ab44e
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-04-06 23:51:41 -0300

    Update Cloud.py

--- app/Cloud/Cloud.py
+++ app/Cloud/Cloud.py
@@ -6,7 +6,10 @@
 import socket
 import json
 import threading # Para Tratar a Concorrência de Requests.
+import datetime
+import math # Para Calcular a Distância Entre o Veículo e o Posto de Recarga.
 from ChargingStationsFile import ChargingStationsFile
+from ChargingPointsFile import ChargingPointsFile
 from ReservationsFile import ReservationsFile
 
 # Garantindo Que o JSON Completo Seja Recebido:
@@ -36,35 +39,72 @@ def handleClient(conn, addr):
             data = receiveFullJson(conn) # Recebendo os Dados do JSON Completos.
             if data:
                 print(f"\nDados Recebidos de '{addr}': {data}\n")
-                # Processamento de Mensagem Recebida do Cliente Posto de Recarga:
-                # CÓDIGO AQUI
+                # Processando Mensagem de Salvar Posto de Recarga no Banco de Dados, Recebida do Cliente "Posto de Recarga":
+                if all(key in data for key in ["newChargingStation", "x_position", "y_position"]): # Verificando Se Todas as Chaves Estão Presentes.
+                    cs = ChargingStationsFile() # Recuperando os Dados do Arquivo ".json".
+                    cs.createChargingStation(data["x_position"], data["y_position"]) # Criando Posto de Recarga no Banco de Dados.
 
-                # Processando Mensagem de Criar uma Reserva, Recebida do Cliente Veículo:
+                # Processando Mensagem de Criar uma Reserva, Recebida do Cliente "Veículo":
                 # Chaves Esperadas: "vid", "x", "y", "actualBatteryPercentage", "batteryCapacity" e "scheduleReservation".
-                if "vid" and "x" and "y" and "actualBatteryPercentage" and "batteryCapacity" and "scheduleReservation" in data: # vid = Vehicle ID.
-                    pass # Falta Fazer Ainda.
-                # Processando Mensagem de Procurar uma Reserva, Recebida do Cliente Veículo:
+                if all(key in data for key in ["vid", "x", "y", "actualBatteryPercentage", "batteryCapacity", "scheduleReservation"]): # vid = Vehicle ID.
+                    # Calculando o Posto de Recarga Mais Próximo:
+                    # Equação de Distância Entre Dois Pontos em um Plano Cartesiano:
+                    # distância = sqrt((xP - xV​)² + (yP - yV​)²), Onde "xV" e "yV" São Coordenadas do Veículo e "xP" e "yP" São Coordenadas do Posto.
+                    cs = ChargingStationsFile()
+                    # Iniciando as Variáveis de Distância e ID do Posto de Recarga Encontrado:
+                    shorterDistance = None
+                    chargingStationID = None
+                    # Comparando a Distância Entre os Posto de Recarga, Para Achar o Mais Próximo:
+                    for station in cs.chargingStationsList:
+                        distanceFromVehicle = math.sqrt((station["x_position"] - data["x"])**2 + (station["y_position"] - data["y"])**2)
+                        if shorterDistance is None or distanceFromVehicle < shorterDistance:
+                            shorterDistance = distanceFromVehicle # Atualizando a Distância do Mais Próximo.
+                            chargingStationID = station["chargingStationID"] # Salvando o ID do Posto de Recarga Encontrado.
+
+                    # AINDA FALTA FAZER:
+                    # Calcular o Ponto de Carregamento Com Reserva Mais Cedo:
+                    """ reservationList = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
+                    reservationList = reservationList.listReservations(chargingStationID) # Lendo Reservas do Posto de Recarga Escolhido.
+                    latestDateTime = datetime.datetime(2100, 12, 31, 0, 0, 0) # Data Base Mais Recente para Comparação.
+                    found = False # Indicará Se um Data Anterior For Encontrada.
+                    for reservation in reservationList:
+                        dateTimeInFile = datetime.datetime.fromisoformat(reservation["finishDateTime"]) # Decodificando a Data na Lista para DateTime.
+                        if dateTimeInFile < latestDateTime:
+                            found = True # Alterando o Status de Data Posterior Encontrada.
+                            latestDateTime = dateTimeInFile
+                    chargingPointID = """
+
+
+                    reservation = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
+                    reservation.createReservation(chargingStationID, chargingPointID, data["vid"], data["actualBatteryPercentage"], data["batteryCapacity"])
+                    # Respondendo Com a Reserva em JSON:
+                    # Chaves Enviadas: "reservationID", "chargingStationID", 
+                    # "chargingPointID", "chargingPointPower", "kWhPrice", 
+                    # "vehicleID", "startDateTime", "finishDateTime", "duration" e "price"
+                    reply = json.dumps(reservation, indent=4).encode('utf-8')
+                    conn.sendall(reply)
+                # Processando Mensagem de Procurar uma Reserva, Recebida do Cliente "Veículo":
                 # Chaves Esperadas: "vid" e "findReservation"
-                elif "vid" and "findReservation" in data: # vid = Vehicle ID.
-                    reservations = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
-                    reservations = reservations.findReservation(data["vehicleID"]) # Procurando a Reserva do Veículo.
-                    if reservations:
+                elif all(key in data for key in ["vid", "findReservation"]): # vid = Vehicle ID.
+                    reservationsFile = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
+                    reservation = reservationsFile.findReservation(data["vid"]) # Procurando a Reserva do Veículo.
+                    if reservation:
                         # Respondendo Com a Reserva em JSON:
                         # Chaves Enviadas: "reservationID", "chargingStationID", 
                         # "chargingPointID", "chargingPointPower", "kWhPrice", 
                         # "vehicleID", "startDateTime", "finishDateTime", "duration" e "price"
-                        reply = json.dumps(reservations, indent=4).encode('utf-8')
+                        reply = json.dumps(reservation, indent=4).encode('utf-8')
                         conn.sendall(reply)
                     # Respondendo Com o Texto "None" em uma String, Se Não Houverem Reservas Para o Veículo:
                     else:
                         conn.sendall(b"None")
-                # Processando Mensagem de Excluir uma Reserva, Recebida do Cliente Veículo:
+                # Processando Mensagem de Excluir uma Reserva, Recebida do Cliente "Veículo":
                 # Chaves Esperadas: "reservationID", "vehicleID" e "deleteReservation".
-                elif "reservationID" and "vehicleID" and "deleteReservation" in data:
-                    reservations = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
-                    reservations = reservations.deleteReservation(data["reservationID"], data["vehicleID"]) # Excluindo a Reserva.
+                elif all(key in data for key in ["reservationID", "vehicleID", "deleteReservation"]): # Verificando Se Todas as Chaves Estão Presentes.
+                    reservationsFile = ReservationsFile() # Lendo as Reservas do Banco de Dados no Arquivo ".json"
+                    reservationDeleted = reservationsFile.deleteReservation(data["reservationID"], data["vehicleID"]) # Excluindo a Reserva.
                     # Reserva Excluida Com Sucesso:
-                    if reservations:
+                    if reservationDeleted:
                         conn.sendall(b"Sucesso")
                     # Reserva Não Encontrada:
                     else:
