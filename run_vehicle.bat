430f5f5244c53692a3fd5246e01e4bfa8cebdf5f
João Macedo <joao.macedo.lima@hotmail.com>
2025-04-07 19:32:40 -0300

    Update directory on .bat

--- run_vehicle.bat
+++ run_vehicle.bat
@@ -4,7 +4,7 @@ echo  Iniciando o container vehicle_client...
 echo =============================================
 
 REM Navega até a pasta onde está o docker-compose.yml
-cd /d "C:\GitHub\ElectricCarsRechargingManager\app\Vehicle"
+cd /d ".\app\Vehicle"
 
 REM Executa o container com terminal interativo
 docker compose run --rm -it --service-ports vehicle
